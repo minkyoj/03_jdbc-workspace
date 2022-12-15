cc0cf1c8999fbee9ec7de6fbc6c3c4af11c940dc
minkyoj <minkyo0816@gmail.com>
2022-12-15 11:06:42 +0900

    1215

--- 99_JDBC_Homework/src/test/TestRun.java
+++ 99_JDBC_Homework/src/test/TestRun.java
@@ -12,35 +12,35 @@ public class TestRun {
 
 	public static void main(String[] args) {
 
-		boolean flag = true;
-
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rset = null;
 		int result = 0;
 		String sql = null;
 		int num = 0;
+		boolean dml = true;
 
-		while (flag = true) {
-			System.out.println("=======+====메뉴선택==============");
-			System.out.println("1. SELECT");
-			System.out.println("2. INSERT");
-			System.out.println("3. DELETE");
-			System.out.println("4. UPDATE");
-			System.out.println("5. 종료");
-			System.out.println("===============================");
-			System.out.print("메뉴번호를 입력해주세요 : ");
-			Scanner sc = new Scanner(System.in);
-			int manu = sc.nextInt();
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
+			stmt = conn.createStatement();
 
-			switch (manu) {
+			while (true) {
+				System.out.println("============메뉴선택==============");
+				System.out.println("1. SELECT");
+				System.out.println("2. INSERT");
+				System.out.println("3. DELETE");
+				System.out.println("4. UPDATE");
+				System.out.println("5. 종료");
+				System.out.println("===============================");
+				System.out.print("메뉴번호를 입력해주세요 : ");
+				Scanner sc = new Scanner(System.in);
+				int menu = sc.nextInt();
+				sc.nextLine();
+				switch (menu) {
 
-			case 1:
-				sql = "SELECT * FROM PRODUCT ORDER BY PNO ASC";
-				try {
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-					stmt = conn.createStatement();
+				case 1: // SELECT
+					sql = "SELECT * FROM PRODUCT ORDER BY PNO ASC";
 					rset = stmt.executeQuery(sql);
 					while (rset.next()) {
 						int pno = rset.getInt("PNO");
@@ -49,157 +49,78 @@ public class TestRun {
 						Date pdate = rset.getDate("REG_DATE");
 						System.out.println("번호 : " + pno + ", 상품명 : " + pname + ", 가격 : " + price + ", 등록일 : " + pdate);
 					}
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						rset.close();
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
+					dml = false;
+					break;
+
+				case 2: // INSERT
+					System.out.print("이름 : ");
+					String name = sc.next();
+					System.out.print("가격 : ");
+					int price = sc.nextInt();
+					sql = "INSERT INTO PRODUCT VALUES(SEQ_PNO.NEXTVAL, '" + name + "', " + price + ", SYSDATE)";
+					dml = true;
+					break;
+
+				case 3: // DELETE
+					System.out.print("삭제할 상품의 번호 : ");
+					num = sc.nextInt();
+					sc.nextLine();
+					sql = "DELETE FROM PRODUCT WHERE PNO = " + num;
+					dml = true;
+					break;
+
+				case 4: // UPDATE
+					System.out.print("수정할 상품의 번호 : ");
+					num = sc.nextInt();
+					sc.nextLine();
+
+					System.out.println("1. 상품명 수정");
+					System.out.println("2. 가격 수정");
+					System.out.print("메뉴번호 입력 : ");
+					int uNum = sc.nextInt();
+
+					sc.nextLine();
+
+					if (uNum == 1) {
+						System.out.print("새로운 상품명 입력 : ");
+						String str = sc.nextLine();
+						sql = "UPDATE PRODUCT SET PNAME = '" + str + "'" + "WHERE PNO = " + num;
+					} else if (uNum == 2) {
+						System.out.print("새로운 가격 입력 : ");
+						int nPrice = sc.nextInt();
+						sql = "UPDATE PRODUCT SET PRICE = " + nPrice + "WHERE PNO = " + num;
 					}
+					dml = true;
+					break;
+
+				case 5: // EXIT
+					System.out.println("종료합니다.");
+					return;
 				}
-				break;
-				
-				
-			case 2:
-				System.out.print("이름 : ");
-				String name = sc.next();
-				System.out.print("가격 : ");
-				int price = sc.nextInt();
-				sql = "INSERT INTO PRODUCT VALUES(SEQ_PNO.NEXTVAL, '" + name + "', " + price + ", SYSDATE)";
-				try {
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-					System.out.println("등록성공");
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-					stmt = conn.createStatement();
+				if (dml == true) {
 					result = stmt.executeUpdate(sql);
 					if (result > 0) {
+						System.out.println("DML 성공");
 						conn.commit();
 					} else {
+						System.out.println("DML 실패");
 						conn.rollback();
-					}
-				} catch (ClassNotFoundException e) {
-					System.out.println("OracleDriver 클래스를 찾지 못했습니다.");
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
+						break;
 					}
 				}
-				if (result > 0) {
-					System.out.println("insert 성공!!");
-				} else {
-					System.out.println("insert 실패");
-				}
-				break;
-				
-			case 3:
-				System.out.print("삭제할 상품의 번호 : ");
-				num = sc.nextInt();
-				sc.nextLine();
-				sql = "DELETE FROM PRODUCT WHERE PNO = " + num;
-				try {
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-					System.out.println("JDBC 드라이버 등록성공");
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-					stmt = conn.createStatement();
-					result = stmt.executeUpdate(sql);
-					if (result > 0) {
-						conn.commit();
-					} else {
-						conn.rollback();
-					}
-				} catch (ClassNotFoundException e) {
-					System.out.println("OracleDriver 클래스를 찾지 못했습니다.");
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				if (result > 0) {
-					System.out.println("Delete 성공!!");
-				} else {
-					System.out.println("Delete 실패");
-				}
-				break;
-			
-			case 4 :
-				System.out.print("수정할 상품의 번호 : ");
-				num = sc.nextInt();
-				sc.nextLine();
-				
-				System.out.println("1. 상품명 수정");
-				System.out.println("2. 가격 수정");
-				System.out.print("메뉴번호 입력 : ");
-				int uNum = sc.nextInt();
-				
-				sc.nextLine();
-				
-				if(uNum == 1) {
-					System.out.print("새로운 상품명 입력 : ");
-					String str = sc.nextLine();
-					sql = "UPDATE PRODUCT SET PNAME = '" + str + "'" + "WHERE PNO = " + num;
-				} else if(uNum == 2) {
-					System.out.print("새로운 가격 입력 : ");
-					int nPrice = sc.nextInt();
-					sql = "UPDATE PRODUCT SET PRICE = " + nPrice + "WHERE PNO = " + num;
-				}
-				
-				try {
-					Class.forName("oracle.jdbc.driver.OracleDriver");
-					System.out.println("JDBC 드라이버 등록성공");
-					conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "JDBC", "JDBC");
-					stmt = conn.createStatement();
-					result = stmt.executeUpdate(sql);
-					if (result > 0) {
-						conn.commit();
-					} else {
-						conn.rollback();
-					}
-				} catch (ClassNotFoundException e) {
-					System.out.println("OracleDriver 클래스를 찾지 못했습니다.");
-					e.printStackTrace();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						stmt.close();
-						conn.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-				if (result > 0) {
-					System.out.println("Update 성공!!");
-				} else {
-					System.out.println("Update 실패");
-				}
-				break;
-			
-			case 5 :
-				System.out.println("종료합니다.");
-				return;
-				
 			}
-			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rset.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
 	}
-
 }
-
